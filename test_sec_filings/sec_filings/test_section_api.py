36c4f5a74485c6d0ac7b5a49fdc148eec95fe74f
natygyoon <59327875+natygyoon@users.noreply.github.com>
2023-01-18 00:38:01 +0900

    Added test coverage for unsupported response schema

--- test_sec_filings/sec_filings/test_section_api.py
+++ test_sec_filings/sec_filings/test_section_api.py
@@ -121,6 +121,36 @@ def test_section_narrative_api_labelstudio(form_type, section, tmpdir):
 
 
 @pytest.mark.parametrize(
+    "form_type, section",
+    [
+        ("10-K", "RISK_FACTORS"),
+        ("10-Q", "RISK_FACTORS"),
+        ("S-1", "RISK_FACTORS"),
+        ("10-K", "_ALL"),
+        ("10-Q", "_ALL"),
+        ("S-1", "_ALL"),
+    ],
+)
+def test_section_narrative_api_with_unsupported_response_schema(form_type, section, tmpdir):
+    sample_document = generate_sample_document(form_type)
+    filename = os.path.join(tmpdir.dirname, "wilderness.xbrl")
+    with open(filename, "w") as f:
+        f.write(sample_document)
+
+    # NOTE(robinson) - Reset the rate limit to avoid 429s in tests
+    app.state.limiter.reset()
+    client = TestClient(app)
+    response = client.post(
+        SECTION_ROUTE,
+        files=[("text_files", (filename, open(filename, "rb"), "text/plain"))],
+        data={"output_schema": "unsupported", "section": [section]},
+    )
+
+    assert response.status_code != 200
+    print(response.json())
+
+
+@pytest.mark.parametrize(
     "form_type",
     [
         ("10-K"),
@@ -319,6 +349,35 @@ def test_section_narrative_api_csv_response(form_type, response_type, section, t
     assert [x["element_type"] for x in response_list]
     assert [x["text"] for x in response_list]
 
+
+@pytest.mark.parametrize(
+    "form_type, response_type, section",
+    [
+        ("10-K", "text/csv", "RISK_FACTORS"),
+        ("10-Q", "text/csv", "RISK_FACTORS"),
+        ("S-1", "text/csv", "RISK_FACTORS"),
+        ("10-K", "text/csv", "_ALL"),
+        ("10-Q", "text/csv", "_ALL"),
+        ("S-1", "text/csv", "_ALL"),
+    ],
+)
+def test_section_narrative_api_csv_response_with_unsupported_response_schema(form_type, response_type, section, tmpdir):
+    sample_document = generate_sample_document(form_type)
+    filename = os.path.join(tmpdir.dirname, "wilderness.xbrl")
+    with open(filename, "w") as f:
+        f.write(sample_document)
+
+    # NOTE(robinson) - Reset the rate limit to avoid 429s in tests
+    app.state.limiter.reset()
+    client = TestClient(app)
+    response = client.post(
+        SECTION_ROUTE,
+        files=[("text_files", (filename, open(filename, "rb"), "text/plain"))],
+        data={"output_format": response_type, "output_schema": "unsupported", "section": [section]},
+    )
+    assert response.status_code != 200
+    print(response.json())
+    
 
 def test_section_narrative_api_health_check():
     client = TestClient(app)
